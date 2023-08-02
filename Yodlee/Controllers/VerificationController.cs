a7ffc65041f701fead2296fd922f7845e438dba2
vasylhulpak <vh@interlogic.com.ua>
2023-08-02 16:31:16 +0300

    Added new Yodlee project

---
+++ Yodlee/Controllers/VerificationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yodlee.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class VerificationController: ControllerBase
+{
+	
+}
---
+++ Yodlee/README.md
@@ -0,0 +1,6 @@
+﻿## Yodlee
+
+### Link to API
+```http request
+https://developer.envestnet.com/products/yodlee/core-apis/docs/api-reference#yodlee-coreapis-accounts-get
+```
