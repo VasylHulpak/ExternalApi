7596b123000a38658de6c0a78f368e4305197b29
vasylhulpak <vh@interlogic.com.ua>
2023-08-03 10:30:31 +0300

    Create controller with callback methods

--- YotiApi/Controllers/YotiController.cs
+++ YotiApi/Controllers/YotiController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using YotiApi.Yoti.Commands;
 
 namespace YotiApi.Controllers;
 
@@ -6,5 +8,23 @@ namespace YotiApi.Controllers;
 [Route("/api/[controller]")]
 public class YotiController : ControllerBase
 {
+	private readonly IMediator _mediator;
+	public YotiController(IMediator mediator)
+	{
+		_mediator = mediator;
+	}
+	
+	[HttpPost]
+	[Route("CallbackIdentityVerification")]
+	public Task CallbackIdentityVerification(CallbackIdentityVerificationCommand command, CancellationToken cancellationToken)
+	{
+		return _mediator.Send(command, cancellationToken);
+	}
 
+	[HttpPost]
+	[Route("CallbackAgeVerification")]
+	public Task CallbackAgeVerification(CallbackAgeVerificationCommand command, CancellationToken cancellationToken)
+	{
+		return _mediator.Send(command, cancellationToken);
+	}
 }
---
+++ YotiApi/Yoti/Commands/CallbackAgeVerificationCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace YotiApi.Yoti.Commands;
+
+public class CallbackAgeVerificationCommand: IRequest
+{
+	
+}
