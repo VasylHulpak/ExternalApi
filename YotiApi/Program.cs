7614c161e091a3dd40f73aebdd5db8c90a0decc1
vasylhulpak <vh@interlogic.com.ua>
2023-08-03 10:30:06 +0300

    Added MediatR to project Yoti

--- YotiApi/Program.cs
+++ YotiApi/Program.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using YotiApi.Helpers;
 using YotiApi.Options;
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(typeof(Program));
 
 var options = builder.Configuration.GetSection("YOTI");
 
